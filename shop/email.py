ddf71ade329cf638cd5f67dc611b651de8fe5838
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2022-10-17 12:16:44 +0300

    Implemented Email thread

--- shop/email.py
+++ shop/email.py
@@ -1,5 +1,18 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+import threading
+
+class EmailThread(threading.Thread):
+
+    def __init__(self,email):
+        self.email = email
+
+        threading.Thread.__init__(self)
+    def run(self):
+        self.email.send()
+
+
+    
 
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
@@ -10,6 +23,8 @@ def send_welcome_email(name,receiver):
     text_content = render_to_string('email/register-email.txt',{"name": name})
     html_content = render_to_string('email/register-email.html',{"name": name})
 
-    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
-    msg.attach_alternative(html_content,'text/html')
-    msg.send()
+    email = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    email.attach_alternative(html_content,'text/html')
+
+    EmailThread(email).start()
+    
